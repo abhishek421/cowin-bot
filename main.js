6b4ea5b7d306d5ec48b577985955c26e5df3b36b
Abhishek Choudhary <64063908+abhishek421@users.noreply.github.com>
2021-05-16 19:21:31 +0530

    Update main.js

--- main.js
+++ main.js
@@ -8,14 +8,21 @@ var inputHandlers = {}
 var stateSelector = document.getElementById("stateSelector")
 var districtSelector = document.getElementById("districtSelector")
 var zipCodeInput = document.getElementById("zipCodeInput")
+var soundToggle = document.getElementById("soundToggle")
+var backgroundToggle = document.getElementById("backgroundToggle")
 var availableSlotsTBody = document.getElementById("availableSlots")
 var bookedSlotsTBody = document.getElementById("bookedSlots")
+var infoSlotsTBody = document.getElementById("infoSlots")
 var availableSlots = []
 var bookedSlots = []
 var totalSlots, totalCenters
 var interval = 5 * 1000
 var intervalRunner
-var audio = new Audio("notification.mp3")
+var audioFake = { play: () => false }
+var audioReal; // init later when needed
+var audio = audioFake;
+var windowActive = true;
+var notification;
 
 // NOTE: THIS VALUE CHANGES ONLY ONCE IN FIVE SECONDS
 // FOR ANYBODY POLLING THE COWIN APIS, I RECOMMEND
@@ -35,7 +42,7 @@ function loadDistricts(cb) {
 			districtSelector.add(new Option(s.district_name, s.district_id))
 		})
 
-		if(cb) cb()
+		if (cb) cb()
 	})
 }
 
@@ -51,7 +58,6 @@ districtSelector.addEventListener('change', (e) => {
 
 	conf.districtId = districtSelector.value
 	findSlots()
-	audio.play()
 })
 
 zipCodeInput.addEventListener('input', (e) => {
@@ -86,6 +92,22 @@ inputHandlers.cost = function (e) {
 	renderSlots()
 }
 
+soundToggle.addEventListener('change', (e) => {
+	// because some browsers need user to interact while playing the audio for the first time
+	if (e.target.checked) {
+		if (!audioReal) audioReal = new Audio("notification.mp3");
+
+		backgroundToggle.checked = true
+		backgroundToggle.disabled = false
+		audio = audioReal
+	} else {
+		backgroundToggle.checked = false
+		backgroundToggle.disabled = true
+		audio = audioFake
+	}
+	audio.play()
+})
+
 function setHidden(q, hidden) {
 	document.querySelectorAll(q).forEach(
 		el => (el.hidden = hidden)
@@ -106,9 +128,9 @@ function req(api, cb) {
 }
 
 function pad(n, p) {
-	n = n+""
-	while(n.length < p) {
-		n = "0"+n
+	n = n + ""
+	while (n.length < p) {
+		n = "0" + n
 	}
 	return n
 }
@@ -144,7 +166,7 @@ function findSlots() {
 		bookedSlots = []
 		totalSlots = 0
 		data.centers.forEach(c => {
-			if(!c.sessions) return;
+			if (!c.sessions) return;
 			c.sessions.forEach(s => {
 				s.available_capacity = Math.floor(s.available_capacity)
 				if (s.available_capacity > 0) {
@@ -184,7 +206,7 @@ function renderSlots() {
 		if (conf.ageGroup && conf.ageGroup != slotData.session.min_age_limit) return;
 		if (conf.brand && conf.brand != slotData.session.vaccine) return;
 		if (conf.cost && conf.cost != slotData.center.fee_type) return;
-		if (conf.zipCodes && conf.zipCodes.length > 0 && conf.zipCodes.indexOf(slotData.center.pincode+"") == -1) return;
+		if (conf.zipCodes && conf.zipCodes.length > 0 && conf.zipCodes.indexOf(slotData.center.pincode + "") == -1) return;
 
 		var tbodyElem = slotData.session.available_capacity ? availableSlotsTBody : bookedSlotsTBody;
 		var newRow = tbodyElem.insertRow();
@@ -195,13 +217,18 @@ function renderSlots() {
 		newRow.insertCell().innerHTML = slotData.session.vaccine;
 		newRow.insertCell().innerHTML = slotData.center.fee_type;
 
-		if(filteredTotalCenters.indexOf(slotData.center.center_id) == -1) filteredTotalCenters.push(slotData.center.center_id);
+		if (filteredTotalCenters.indexOf(slotData.center.center_id) == -1) filteredTotalCenters.push(slotData.center.center_id);
 		filteredTotalSlots += slotData.session.available_capacity;
 	})
 
+	infoSlotsTBody.hidden = (filteredTotalSlots > 0)
+
 	document.getElementById("summary").innerText = `Centers: ${filteredTotalCenters.length} | Available vaccines: ${filteredTotalSlots}`;
 
-	if(filteredTotalSlots>0) audio.play();
+	if (filteredTotalSlots > 0) {
+		audio.play();
+		// notifyUser(`${filteredTotalSlots} Vaccine slots found!`, `Book on cowin.gov.in before it's gone`)
+	}
 
 	saveConfig();
 }
@@ -212,20 +239,20 @@ function saveConfig() {
 
 function loadConfig() {
 	var defaultConfig = localStorage.getItem("defaultConfig")
-	if(!defaultConfig) return;
+	if (!defaultConfig) return;
 
 	defaultConfig = JSON.parse(defaultConfig);
 
 	conf = defaultConfig || {}
 	stateSelector.value = conf.stateId
-	if(!conf.stateId) return;
+	if (!conf.stateId) return;
 	loadDistricts(() => {
-		if(!conf.districtId) return;
+		if (!conf.districtId) return;
 		districtSelector.value = conf.districtId
-		if(conf.zipCodes && conf.zipCodes instanceof Array) zipCodeInput.value = conf.zipCodes.join(",")
-		document.querySelectorAll(`input[type=radio][name=ageGroup][value="${conf.ageGroup||'0'}"]`)[0].checked = true
-		document.querySelectorAll(`input[type=radio][name=brand][value="${conf.brand||''}"]`)[0].checked = true
-		document.querySelectorAll(`input[type=radio][name=cost][value="${conf.cost||''}"]`)[0].checked = true
+		if (conf.zipCodes && conf.zipCodes instanceof Array) zipCodeInput.value = conf.zipCodes.join(",")
+		document.querySelectorAll(`input[type=radio][name=ageGroup][value="${conf.ageGroup || '0'}"]`)[0].checked = true
+		document.querySelectorAll(`input[type=radio][name=brand][value="${conf.brand || ''}"]`)[0].checked = true
+		document.querySelectorAll(`input[type=radio][name=cost][value="${conf.cost || ''}"]`)[0].checked = true
 
 		findSlots()
 	})
@@ -237,7 +264,23 @@ function startWatcher() {
 
 	findSlots()
 	window.clearInterval(intervalRunner)
-	intervalRunner = window.setInterval(findSlots, interval);
+	intervalRunner = window.setInterval(() => {
+		findSlots()
+	}, interval);
+}
+
+function notifyUser(title, body) {
+	if (windowActive) return;
+
+	if (!("Notification" in window)) {
+		return;
+	}
+
+	else if (Notification.permission === "granted") {
+		if (!title) return;
+		if (notification) notification.close()
+		notification = new Notification(title, {body});
+	}
 }
 
 document.querySelectorAll('input[type=radio]').forEach(
@@ -255,10 +298,16 @@ req(apiStates, (status, body) => {
 	loadConfig()
 })
 
-window.addEventListener('focus', startWatcher);
+window.addEventListener('focus', () => {
+	windowActive = true;
+	startWatcher();
+});
 startWatcher()
 
 window.addEventListener('blur', () => {
+	windowActive = false;
+	if (backgroundToggle.checked) return; // keep running the watcher
+
 	setHidden("#offlineIndicator", false)
 	setHidden("#liveIndicator", true)
 
